e586432322f6822eec696c0bd598660df2058f69
Javier <jmoreeno18@gmail.com>
2024-11-27 19:47:48 +0100

    Cambio de title

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -14,7 +14,7 @@ export const GifGrid = ({ category }) => {
 
       <h3>{category}</h3>
       {
-        isLoading && <h2>Cargando...</h2>
+        isLoading && <h2>Cargando....</h2>
       }
 
       <div className="card-grid">
